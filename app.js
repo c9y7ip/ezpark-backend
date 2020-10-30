bf59cc76186d06e41a140f8177b8ecec1ffcb9a0
jorawarc <jorawarc@sfu.ca>
2020-10-29 19:05:14 -0700

    Update mongoDB url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 // connect mongoDB
 mongoose.connect(
-  'localhost:27017',
+  'mongodb://localhost:27017',
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 

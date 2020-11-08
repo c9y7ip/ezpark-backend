bb296cb17d5c0b441b66406f2b77c340b0ecc560
lgoldber <lgoldber@sfu.ca>
2020-11-08 12:51:20 -0800

    add firstname and lastname field to schema, make it required, login returns firstname as well now

--- routes/auth.js
+++ routes/auth.js
@@ -11,72 +11,74 @@ router.get('/', (req, res) => {
 
 
 router.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try{
+  try {
     const users = await User.find()
     res.json(users)
   } catch (e) {
-    res.status(500).json({'message': e.message});
+    res.status(500).json({ 'message': e.message });
   }
 })
 
 router.post('/register', async (req, res) => {
   try {
-    let userExists = await User.find({email: req.body.email});
-    if(Array.isArray(userExists) && userExists.length){
+    let userExists = await User.find({ email: req.body.email });
+    if (Array.isArray(userExists) && userExists.length) {
       res.redirect('/login'); // TODO: notify user existence
-    } else{
-        const user = new User({
-          name: req.body.name,
-          password: await bcrypt.hash(req.body.password, 10),
-          email: req.body.email,
-          phone: req.body.phone,
-          isAdmin: JSON.parse(req.body.isAdmin) // Convert admin to boolean
-        });
+    } else {
+      const user = new User({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: await bcrypt.hash(req.body.password, 10),
+        email: req.body.email,
+        phone: req.body.phone,
+        isAdmin: JSON.parse(req.body.isAdmin) // Convert admin to boolean
+      });
 
-        const newUser = await user.save();
-        res.status(201).json(newUser)
-      }
-    } catch (error){
+      const newUser = await user.save();
+      res.status(201).json(newUser)
+    }
+  } catch (error) {
     console.log(error);
     res.redirect('/'); // Error Page
   }
 })
 
 router.post('/login',
-    async (req, res, next) => {
-        passport.authenticate(
-            'local',
-            async (err, user, info) => {
-                try {
-                    if (err || !user) {
-                        res.redirect('/login');
-                        return next(err);
-                    }
+  async (req, res, next) => {
+    passport.authenticate(
+      'local',
+      async (err, user, info) => {
+        try {
+          if (err || !user) {
+            res.redirect('/login');
+            return next(err);
+          }
 
-                    req.login(
-                        user,
-                        { session: false },
-                        async (error) => {
-                            if (error) return next(error);
+          req.login(
+            user,
+            { session: false },
+            async (error) => {
+              if (error) return next(error);
+              const body = { _id: user._id, email: user.email };
+              const token = jwt.sign({ user: body }, process.env.SECRET || 'secretKey');
+              console.log(token)
 
-                            const body = { _id: user._id, email: user.email };
-                            const token = jwt.sign({ user: body }, process.env.SECRET || 'secretKey');
-                            console.log(token)
+              const firstname = user.firstname
 
-                            res.send(`Bearer ${token}`);
-                        }
-                    );
-                } catch (error) {
-                    return next(error);
-                }
+              res.json({ token: `Bearer ${token}`, name: firstname });
             }
-        )(req, res, next);
-    }
+          );
+        } catch (error) {
+          return next(error);
+        }
+      }
+    )(req, res, next);
+  }
 );
 
 
 router.post('/logout', ((req, res) => {
-  res.cookie('jwt', {expires: Date.now()});
+  res.cookie('jwt', { expires: Date.now() });
   res.redirect('/login');
 }))
 

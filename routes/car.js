d6f7e8ec75107a0f3f8b8dc8761fee6aba27c676
jorawarc <jorawarc@sfu.ca>
2020-11-14 15:38:34 -0800

    Insert car if does not exist otherwise error

--- routes/car.js
+++ routes/car.js
@@ -10,12 +10,22 @@ router.get('/', (req, res) => {
 
 router.post('/add', async (req, res) => {
   const payload = jwtDecode(req.header('authorization'));
-  await Car.create(Object.assign({createdBy: payload['user']['_id']}, req.body)).then((success) => {
-    res.json(req.body);
+  const update = {$setOnInsert: Object.assign({createdBy: payload['user']['_id']}, req.body)}
+  await Car.findOneAndUpdate({createdBy: payload['user']['_id'], license: req.body.license}, update,
+      {upsert: true, new: true, runValidators: true, rawResult: true}).then(
+      (result) => {
+        if(result.lastErrorObject.updatedExisting){
+          console.log("Car already exists")
+          res.status(409).json(req.body)
+        } else {
+          console.log("Successfully added a new car")
+          res.status(200).json(req.body)
+        }
   }, (error) => {
     res.status(400)
   })
 })
+
 
 router.delete('/delete', async (req, res) => {
   const payload = jwtDecode(req.header('authorization'));
@@ -25,6 +35,7 @@ router.delete('/delete', async (req, res) => {
     res.status(409).json(req.body);
   })
 })
+
 
 router.put('/edit', async (req, res) => {
   const payload = jwtDecode(req.header('authorization'));

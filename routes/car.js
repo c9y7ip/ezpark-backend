33b3a46e45e88b4259e167ba1f6400e87a0a5b80
jorawarc <jorawarc@sfu.ca>
2020-11-14 16:05:03 -0800

    Implement find

--- routes/car.js
+++ routes/car.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Car = require('../models/car');
+const User = require('../models/user');
 const jwtDecode = require('jwt-decode');
 
 router.get('/', (req, res) => {
@@ -31,7 +32,7 @@ router.delete('/delete', async (req, res) => {
   const payload = jwtDecode(req.header('authorization'));
   await Car.findOneAndDelete({license: req.body.license, createdBy: payload['user']['_id']}).then((success) => {
     if (success === null){
-      res.status(404).json(req.body)
+      res.status(404).json(req.body) // Could not find document
     } else{
       res.json(success)
     }
@@ -46,14 +47,35 @@ router.put('/edit', async (req, res) => {
   await Car.update({license: req.body.license, createdBy: payload['user']['_id']}, req.body).then((success) => {
     if (success.nModified === 0){
       console.log("No documents modified")
-      res.status(404).json(req.body)
+      res.status(404).json(req.body) // No changes
     } else {
-      res.json(req.body);
+      res.json(req.body); // Document updated
     }
   }, (error) => {
-    res.status(404).json(req.body);
+    res.status(404).json(req.body); // Server error
   })
 })
 
+
+router.post('/find', async (req, res) => {
+  const payload = jwtDecode(req.header('authorization'));
+  await User.findById(payload['user']['_id']).then(async (success) => {
+        console.log(success)
+        if(success.isAdmin === true){
+          await Car.findOne({license: req.body.license}).then((success) => {
+            console.log(success)
+            if(success !== null){
+              res.status(200).json(success) // Car found with license
+            } else {
+              res.status(204).send() // No car found with matching license
+            }
+          })
+        } else {
+          res.status(403).json(req.body) // User is not an admin
+        }
+      }, (error) => {
+    res.status(404).json(req.body) // Server error
+  })
+})
 
 module.exports = router;
